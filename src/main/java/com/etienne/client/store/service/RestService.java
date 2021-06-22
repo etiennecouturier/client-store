88dfcb13e9465c23539cae0d025090be8388a16d
etiennecouturier <iistvan.szabo2@gmail.com>
2021-06-22 08:58:37 +0200

    add settings.gradle

--- src/main/java/com/etienne/client/store/service/RestService.java
+++ src/main/java/com/etienne/client/store/service/RestService.java
@@ -1,5 +1,6 @@
 package com.etienne.client.store.service;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
@@ -8,15 +9,18 @@ import org.springframework.web.client.RestTemplate;
 @Service
 public class RestService {
 
+    @Value("${client.store.address}")
+    private String address;
+
     private final RestTemplate restTemplate;
 
     public RestService(RestTemplateBuilder restTemplateBuilder) {
         this.restTemplate = restTemplateBuilder.build();
     }
 
-    @Scheduled(cron = "${cron.keep.alive}")
+    @Scheduled(cron = "${client.store.cron.keep.alive}")
     public void ping() {
-        restTemplate.getForObject("https://clients-store.herokuapp.com/ping", String.class);
+        restTemplate.getForObject(address + "/ping", String.class);
     }
 
 }
