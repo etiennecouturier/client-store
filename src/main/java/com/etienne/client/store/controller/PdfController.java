fbb6313bae3a0613fe7c8de04148d124aff374b7
etiennecouturier <iistvan.szabo2@gmail.com>
2021-07-04 19:53:04 +0200

    add settings.gradle

--- src/main/java/com/etienne/client/store/controller/PdfController.java
+++ src/main/java/com/etienne/client/store/controller/PdfController.java
@@ -7,10 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.InputStreamResource;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
 import java.util.List;
@@ -18,6 +15,7 @@ import java.util.List;
 @RestController
 @RequestMapping(path = "/pdf")
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
+@CrossOrigin(origins = "http://localhost:4200")
 public class PdfController {
 
     private final PdfService pdfService;
