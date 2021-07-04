cd97e9d2a9055ea534386582ef7c50160004e670
etiennecouturier <iistvan.szabo2@gmail.com>
2021-07-04 19:09:39 +0200

    add settings.gradle

--- src/main/java/com/etienne/client/store/controller/PdfController.java
+++ src/main/java/com/etienne/client/store/controller/PdfController.java
@@ -1,9 +1,14 @@
 package com.etienne.client.store.controller;
 
+import com.etienne.client.store.model.exception.ClientNotFoundException;
 import com.etienne.client.store.service.PdfService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,6 +25,15 @@ public class PdfController {
     @GetMapping(path = "/fields")
     public List<String> findFields() throws IOException {
         return pdfService.findFields();
+    }
+
+    @GetMapping(path = "/download/{clientId}", produces = MediaType.APPLICATION_PDF_VALUE)
+    public ResponseEntity<InputStreamResource> downloadPdf(@PathVariable String clientId) throws IOException, ClientNotFoundException {
+        return ResponseEntity
+                .ok()
+                .body(new InputStreamResource(
+                        pdfService.downloadPdf(clientId)
+                ));
     }
 
 }
--- src/main/java/com/etienne/client/store/service/PdfService.java
+++ src/main/java/com/etienne/client/store/service/PdfService.java
@@ -1,34 +1,59 @@
 package com.etienne.client.store.service;
 
+import com.etienne.client.store.model.domain.Client;
+import com.etienne.client.store.model.exception.ClientNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.interactive.form.PDAcroForm;
 import org.apache.pdfbox.pdmodel.interactive.form.PDField;
-import org.apache.pdfbox.pdmodel.interactive.form.PDTerminalField;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.util.List;
-import java.util.stream.Collectors;
 
 import static java.util.Spliterator.ORDERED;
 import static java.util.Spliterators.spliteratorUnknownSize;
+import static java.util.stream.Collectors.toList;
 import static java.util.stream.StreamSupport.stream;
 import static org.apache.pdfbox.pdmodel.PDDocument.load;
 
 @Service
+@RequiredArgsConstructor(onConstructor = @__(@Autowired))
 public class PdfService {
 
+    private final ClientService clientService;
+
     public List<String> findFields() throws IOException {
-        return stream(
-                spliteratorUnknownSize(
-                        load(PdfService.class.getResourceAsStream("/form.pdf"))
-                                .getDocumentCatalog()
-                                .getAcroForm()
-                                .getFieldTree()
-                                .iterator(), ORDERED
-                ), false
-        ).filter(pdField -> pdField instanceof PDTerminalField)
+        return stream(spliteratorUnknownSize(
+                getAcroForm()
+                        .getFieldTree()
+                        .iterator(), ORDERED), false)
                 .map(PDField::getFullyQualifiedName)
-                .collect(Collectors.toList());
+                .collect(toList());
     }
 
+    public ByteArrayInputStream downloadPdf(String clientId) throws ClientNotFoundException, IOException {
+        Client client = clientService.getClientById(clientId);
+        PDDocument pdf = load(PdfService.class.getResourceAsStream("/form.pdf"));
+        PDAcroForm form = pdf
+                .getDocumentCatalog()
+                .getAcroForm();
+        form.getField("name").setValue(client.getName());
+        form.getField("tel").setValue(client.getTel());
+
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        pdf.save(out);
+        pdf.close();
+        return new ByteArrayInputStream(out.toByteArray());
+    }
+
+    private PDAcroForm getAcroForm() throws IOException {
+        return load(PdfService.class.getResourceAsStream("/form.pdf"))
+                .getDocumentCatalog()
+                .getAcroForm();
+    }
 
 }
