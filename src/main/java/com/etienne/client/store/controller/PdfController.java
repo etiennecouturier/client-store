adc263f499e044cf56ff59d703fc5e7e41b7243d
etiennecouturier <iistvan.szabo2@gmail.com>
2021-07-06 09:56:09 +0200

    add settings.gradle

--- src/main/java/com/etienne/client/store/controller/PdfController.java
+++ src/main/java/com/etienne/client/store/controller/PdfController.java
@@ -10,7 +10,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
-import java.util.List;
 
 import static org.springframework.http.MediaType.APPLICATION_PDF_VALUE;
 
@@ -22,13 +21,8 @@ public class PdfController {
 
     private final PdfService pdfService;
 
-    @GetMapping(path = "/fields")
-    public List<String> findFields() throws IOException {
-        return pdfService.findFields();
-    }
-
     @GetMapping(path = "/download/{visitId}", produces = APPLICATION_PDF_VALUE)
-    public ResponseEntity<InputStreamResource> downloadPdf(@PathVariable ObjectId visitId) throws IOException, ClientNotFoundException {
+    public ResponseEntity<InputStreamResource> downloadPdf(@PathVariable ObjectId visitId) throws IOException {
         return ResponseEntity
                 .ok()
                 .body(new InputStreamResource(
--- src/main/java/com/etienne/client/store/service/PdfService.java
+++ src/main/java/com/etienne/client/store/service/PdfService.java
@@ -2,24 +2,23 @@ package com.etienne.client.store.service;
 
 import com.etienne.client.store.model.domain.ClientVisit;
 import lombok.RequiredArgsConstructor;
+import org.apache.pdfbox.Loader;
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.pdmodel.PDResources;
 import org.apache.pdfbox.pdmodel.font.PDType0Font;
 import org.apache.pdfbox.pdmodel.interactive.form.PDAcroForm;
-import org.apache.pdfbox.pdmodel.interactive.form.PDField;
 import org.apache.pdfbox.pdmodel.interactive.form.PDTextField;
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.io.*;
-import java.util.List;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 
 import static java.util.Spliterator.ORDERED;
 import static java.util.Spliterators.spliteratorUnknownSize;
-import static java.util.stream.Collectors.toList;
 import static java.util.stream.StreamSupport.stream;
-import static org.apache.pdfbox.pdmodel.PDDocument.load;
 
 @Service
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
@@ -27,19 +26,10 @@ public class PdfService {
 
     private final ClientService clientService;
 
-    public List<String> findFields() throws IOException {
-        return stream(spliteratorUnknownSize(
-                getAcroForm()
-                        .getFieldTree()
-                        .iterator(), ORDERED), false)
-                .map(PDField::getFullyQualifiedName)
-                .collect(toList());
-    }
-
     public ByteArrayInputStream downloadPdf(ObjectId visitId) throws IOException {
         ClientVisit client = clientService.findClientWithVisit(visitId);
 
-        PDDocument pdf = load(PdfService.class.getResourceAsStream("/form.pdf"));
+        PDDocument pdf = Loader.loadPDF(PdfService.class.getResourceAsStream("/form.pdf"));
         PDType0Font font = PDType0Font.load(pdf, PdfService.class.getResourceAsStream("/fonts/Calibri-Light.ttf"), true);
         PDResources res = pdf.getDocumentCatalog().getAcroForm().getDefaultResources();
         String fontName = res.add(font).getName();
@@ -75,12 +65,6 @@ public class PdfService {
         pdf.save(out);
         pdf.close();
         return new ByteArrayInputStream(out.toByteArray());
-    }
-
-    private PDAcroForm getAcroForm() throws IOException {
-        return load(PdfService.class.getResourceAsStream("/form.pdf"))
-                .getDocumentCatalog()
-                .getAcroForm();
     }
     
     private <T> String toStr(T obj) {
