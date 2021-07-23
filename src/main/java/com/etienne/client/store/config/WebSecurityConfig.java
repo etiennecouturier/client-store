0a7f8eb64146511a803dec617fa1f9739c276b58
etiennecouturier <iistvan.szabo2@gmail.com>
2021-07-23 13:33:39 +0200

    add settings.gradle

--- src/main/java/com/etienne/client/store/config/WebSecurityConfig.java
+++ src/main/java/com/etienne/client/store/config/WebSecurityConfig.java
@@ -27,7 +27,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     public void configure(WebSecurity web) throws Exception {
-        web.ignoring().antMatchers("resources", "/static/**");
+        web.ignoring().antMatchers("/resources/**", "/static/**");
     }
 
     @Override
